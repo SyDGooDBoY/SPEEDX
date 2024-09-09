1baea2f3aee1f0627602bcab784142b91d2b6430
Alexxxxx <61483771+TosakaReina@users.noreply.github.com>
2024-09-09 15:32:57 +1000

    update energy bar with new stages

--- GDS2Project/Assets/Scripts/UI/StaminaBar.cs
+++ GDS2Project/Assets/Scripts/UI/StaminaBar.cs
@@ -29,20 +29,22 @@ public class StaminaBar : MonoBehaviour
         float currentEnergy = energySystem.GetCurrentEnergy();
         float maxEnergy = AbilityManager.HIGH_THRESHOLD;
 
+
+
         float energyPercentage = currentEnergy / maxEnergy;
 
-        float redWidth = Mathf.Clamp01(energyPercentage / 0.25f) * totalWidth * 0.25f;
+        float redWidth = Mathf.Clamp01(energyPercentage / 0.5f) * totalWidth * 0.5f;
 
         float yellowWidth = 0;
-        if (energyPercentage > 0.25f)
+        if (energyPercentage > 0.50f)
         {
-            yellowWidth = Mathf.Clamp01((energyPercentage - 0.25f) / 0.25f) * totalWidth * 0.25f;
+            yellowWidth = Mathf.Clamp01((energyPercentage - 0.50f) / 0.25f) * totalWidth * 0.25f;
         }
 
         float greenWidth = 0;
-        if (energyPercentage > 0.50f)
+        if (energyPercentage > 0.75f)
         {
-            greenWidth = Mathf.Clamp01((energyPercentage - 0.50f) / 0.50f) * totalWidth * 0.50f;
+            greenWidth = Mathf.Clamp01((energyPercentage - 0.75f) / 0.25f) * totalWidth * 0.25f;
         }
 
         redBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, redWidth);
