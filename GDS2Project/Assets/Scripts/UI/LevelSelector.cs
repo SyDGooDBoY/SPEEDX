950a0b9640aed80ac652ddd10eedc755a1960e81
SyDGooDBoY <Hongfei.Wei@student.uts.edu.au>
2024-09-20 21:57:05 +1000

    Add possible code for level selector

--- GDS2Project/Assets/Scripts/UI/LevelSelector.cs
+++ GDS2Project/Assets/Scripts/UI/LevelSelector.cs
@@ -22,4 +22,42 @@ public class LevelSelector : MonoBehaviour
     {
         SceneManager.LoadScene("Level " + level.ToString());
     }
+    
+    
+    
+    //NOT SURE IF THE CODE BELOW IS RIGHT OR NOT
+    
+    //
+    // public int level; // Level number
+    // public TextMeshProUGUI levelText;
+    // public Button levelButton; // Reference to the Button component
+    //
+    // void Start()
+    // {
+    //     levelText.text = "Level " + level;
+    //     CheckLevelUnlocked();
+    // }
+    //
+    // void CheckLevelUnlocked()
+    // {
+    //     // Check if the level is unlocked using SaveManager
+    //     if (SaveManager.Instance.IsLevelUnlocked("Level " + level))
+    //     {
+    //         levelButton.interactable = true;
+    //     }
+    //     else
+    //     {
+    //         levelButton.interactable = false;
+    //         // Optionally set a different text or color to indicate it's locked
+    //         levelText.color = Color.gray; // Change color to gray if locked
+    //     }
+    // }
+    //
+    // public void OpenLevel()
+    // {
+    //     if (levelButton.interactable)
+    //     {
+    //         SceneManager.LoadScene("Level " + level);
+    //     }
+    // }
 }
