d2a362cdf8a03474e78697762793361f1ce50954
SyDGooDBoY <Hongfei.Wei@student.uts.edu.au>
2024-09-20 17:21:47 +1000

    Adjust teleport gun

--- GDS2Project/Assets/Scripts/Player/PlayerShootTeleport.cs
+++ GDS2Project/Assets/Scripts/Player/PlayerShootTeleport.cs
@@ -13,6 +13,9 @@ public class PlayerShootTeleport : MonoBehaviour
     public float cooldown = 2.0f;
 
     private float lastShootTime = 0;
+    public float targetY = 0.5f; // 用于调整抛物线的高度
+
+    public float targetX = 0.05f; // 用于调整抛物线的水平偏移
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +35,14 @@ public class PlayerShootTeleport : MonoBehaviour
 
     void Shoot()
     {
-        var bullet = Instantiate(ballPrefab, shootingPoint.position, shootingPoint.rotation);
-        bullet.GetComponent<Rigidbody>().velocity = shootingPoint.forward * ballSpeed;
+        var bullet = Instantiate(ballPrefab, shootingPoint.position, Quaternion.identity);
+        var rb = bullet.GetComponent<Rigidbody>();
+
+
+        Vector3 targetDir = shootingPoint.forward;
+        targetDir.y += targetY; // 给一个向上的初始偏移，让抛物线弯曲更明显
+        targetDir.x += targetX;
+
+        rb.AddForce(targetDir.normalized * ballSpeed, ForceMode.VelocityChange);
     }
 }
