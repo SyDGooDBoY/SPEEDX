229a0d693abadaf6a7a203c3fd083423c1fc5a3a
SyDGooDBoY <Hongfei.Wei@student.uts.edu.au>
2024-09-20 17:02:37 +1000

    Add teleportgun cd

--- GDS2Project/Assets/Scripts/Player/PlayerShootTeleport.cs
+++ GDS2Project/Assets/Scripts/Player/PlayerShootTeleport.cs
@@ -10,18 +10,29 @@ public class PlayerShootTeleport : MonoBehaviour
 
     public float ballSpeed = 10.0f;
 
+    public float cooldown = 2.0f;
+
+    private float lastShootTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        lastShootTime = -cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && Time.time - lastShootTime >= cooldown)
         {
-            var bullet = Instantiate(ballPrefab, shootingPoint.position, shootingPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = shootingPoint.forward * ballSpeed;
+            Shoot();
+            lastShootTime = Time.time;
         }
+    }
+
+    void Shoot()
+    {
+        var bullet = Instantiate(ballPrefab, shootingPoint.position, shootingPoint.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = shootingPoint.forward * ballSpeed;
     }
 }
