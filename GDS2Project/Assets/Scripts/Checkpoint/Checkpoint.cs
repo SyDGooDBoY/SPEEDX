5e1235cbb8717f3c3d07de175d2643c9cee990dd
SyDGooDBoY <Hongfei.Wei@student.uts.edu.au>
2024-10-27 20:59:43 +1100

    update check point sound and vfx

--- GDS2Project/Assets/Scripts/Checkpoint/Checkpoint.cs
+++ GDS2Project/Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -4,6 +4,10 @@ public class Checkpoint : MonoBehaviour
 {
     public GameObject camLookAt;
     private Transform camLookAtTrans;
+    private MeshRenderer mesh;
+    private AudioSource audioSource;
+    private AudioClip checkpointSound;
+    private bool checkpointReached;
 
     private void Start()
     {
@@ -15,15 +19,30 @@ public class Checkpoint : MonoBehaviour
         {
             camLookAtTrans = camLookAt.transform;
         }
+
+        mesh = GetComponent<MeshRenderer>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+        }
+        else
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        checkpointSound = Resources.Load<AudioClip>("Sound/save");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !checkpointReached)
         {
+            audioSource.PlayOneShot(checkpointSound);
+            checkpointReached = true;
             CheckpointManager.UpdateCheckpoint(transform.position, camLookAtTrans);
+            mesh.enabled = false;
             Debug.Log("Checkpoint reached.");
         }
     }
 }
-
